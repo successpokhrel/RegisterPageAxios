6a8fa3f857c17bd45a89cc8eb2590b3d00e82274
successpokhrel <successpokhrel25@gmail.com>
2024-12-18 11:28:57 +0545

    displaying error message

--- src/Register.jsx
+++ src/Register.jsx
@@ -23,11 +23,36 @@ const Register = () =>{
 
     const [errMsg, setErrMsg] = useState('');
     const [success, setSuccess] = useState(false);
+
+    useEffect(()=>{
+        userRef.current.focus();
+    }, [])
+
+    useEffect(()=>{
+        const result = USER_REGEX.test(user);
+        console.log(result);
+        console.log(user);
+        setValidName(result);
+    }, [user])
+
+    useEffect(()=>{
+        const result = PWD_REGEX.test(pwd);
+        console.log(result);
+        console.log(pwd);
+        setValidPwd(result);
+        const match = pwd === matchPwd;
+        setValidMatch(match);
+    }, [pwd, matchPwd])
+
+    useEffect(()=>{
+        setErrMsg('');
+    }, [user, pwd, matchPwd])
+    
  
     return (
-        <div>
-
-        </div>
+        <section>
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+        </section>
     )
 }
 
