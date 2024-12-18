04f2229c503bb03564964d71a7ed949f306acc28
successpokhrel <successpokhrel25@gmail.com>
2024-12-18 11:47:31 +0545

    password field

--- src/Register.jsx
+++ src/Register.jsx
@@ -63,9 +63,15 @@ const Register = () => {
       </p>
       <form>
         <label htmlFor="username">
-        Username:
-            <FontAwesomeIcon icon={faCheck} className={validName ? "valid" : "hide"} />
-            <FontAwesomeIcon icon={faTimes} className={validName || !user ? "hide" : "invalid"} />
+          Username:
+          <FontAwesomeIcon
+            icon={faCheck}
+            className={validName ? "valid" : "hide"}
+          />
+          <FontAwesomeIcon
+            icon={faTimes}
+            className={validName || !user ? "hide" : "invalid"}
+          />
         </label>
         <input
           type="text"
@@ -94,7 +100,47 @@ const Register = () => {
           Letters, numbers, underscores, hyphens allowed.
         </p>
 
+        <label htmlFor="password">
+          Password:
+          <FontAwesomeIcon
+            icon={faCheck}
+            className={validPwd ? "valid" : "hide"}
+          />
+          <FontAwesomeIcon
+            icon={faTimes}
+            className={validPwd || !pwd ? "hide" : "invalid"}
+          />
+        </label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPwd(e.target.value)}
+          value={pwd}
+          required
+          aria-invalid={validPwd ? "false" : "true"}
+          aria-describedby="pwdnote"
+          onFocus={() => setPwdFocus(true)}
+          onBlur={() => setPwdFocus(false)}
+        />
+        <p
+          id="pwdnote"
+          className={pwdFocus && !validPwd ? "instructions" : "offscreen"}
+        >
+          <FontAwesomeIcon icon={faInfoCircle} />
+          8 to 24 characters.
+          <br />
+          Must include uppercase and lowercase letters, a number and a special
+          character.
+          <br />
+          Allowed special characters:{" "}
+          <span aria-label="exclamation mark">!</span>{" "}
+          <span aria-label="at symbol">@</span>{" "}
+          <span aria-label="hashtag">#</span>{" "}
+          <span aria-label="dollar sign">$</span>{" "}
+          <span aria-label="percent">%</span>
+        </p>
 
+        
       </form>
     </section>
   );
